a39c6f9d47ab4fec3f7ea866c79eca4af16a0a78
leowitcroz <leowitcroz@gmail.com>
2023-09-28 21:44:51 -0300

    finish project

--- main.py
+++ main.py
@@ -7,16 +7,33 @@ st.title('Weather Forecast for the Next Days')
 place = st.text_input('Place: ')
 days = st.slider('Forecast Days', min_value=1, max_value=5, help='Select the number of forecasted days')
 
-option = st.selectbox('Select data to view', ('Temperature','sky'))
+option = st.selectbox('Select data to view', ('Temperature','Sky'))
 
 st.subheader(f' {option} for the next {days} days in {place}')
 
-data = get_data(place,days,option)
 
-dates=['2022-25-10','2022-26-10','2022-27-10']
-temperatures = [10,11,15]
-
-
-
-figure = px.line(x=dates, y=temperatures, labels={'x':'Dates', 'y':'Temperature (C°)'})
-st.plotly_chart(figure)
+if place:
+    
+    try:
+  
+        filtered_data = get_data(place,days)
+        
+        print(dict['dt_temp'][0] for dict in filtered_data)
+        
+        if option == 'Temperature':
+            temperatures = [dict['main']['temp']/10 for dict in filtered_data]
+            date = [dict['dt_txt'] for dict in filtered_data]
+            
+            figure = px.line(x=date, y=temperatures, labels={'x':'Dates', 'y':'Temperature (C°)'})
+            st.plotly_chart(figure)
+            
+        if option == 'Sky':
+            images = {'Clear': 'images/clear.png', 'Clouds':'images/cloud.png','Rain':'images/rain.png', 'Snow':'images/snow.png'}
+            date = [dict['dt_txt'] for dict in filtered_data]
+            
+            sky_condition = [dict['weather'][0]['main'] for dict in filtered_data]
+            images_paths = [images[condition] for condition in sky_condition]
+            
+            st.image(images_paths,width=115,caption=date)
+    except KeyError:
+        st.write('That Place does not exist')
